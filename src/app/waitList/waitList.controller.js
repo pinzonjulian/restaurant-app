86dfd6852dad631cdb86625e8629cbbf111bb89a
pinzonjulian <julianpinzoneslava@gmail.com>
2016-02-12 11:56:56 -0500

    Create Party constructor in waitListController.js

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -11,12 +11,21 @@
       var vm = this;
 
       var fireParties = new Firebase('https://jperestaurantapp.firebaseio.com/parties');
-      vm.parties = $firebaseArray(fireParties);
 
+      function Party () {
+        this.name = '';
+        this.phone = '';
+        this.size = '';
+        this.done = false;
+        this.notified = false;
+      }
+
+      vm.newParty = new Party();
+      vm.parties = $firebaseArray(fireParties);
       vm.addParty = addParty;
 
       function addParty() {
-        vm.parties.$add('another');
+        vm.parties.$add(vm.newParty);
       }
 
     }
