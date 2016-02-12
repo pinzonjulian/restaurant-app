79265bc4840d19944f288cd3154574cef749dc71
pinzonjulian <julianpinzoneslava@gmail.com>
2016-02-12 16:51:05 -0500

    Add toggleDone function and ng-change directive to Done checkbox

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -26,6 +26,7 @@
       vm.addParty = addParty;
       vm.removeParty = removeParty;
       vm.sendTextMessage = sendTextMessage;
+      vm.toggleDone = toggleDone;
 
 
       function addParty() {
@@ -45,6 +46,10 @@
         };
         fireTextMessages.push(newTextMessage);
         party.notified = true;
+        vm.parties.$save(party);
+      }
+
+      function toggleDone (party) {
         vm.parties.$save(party);
       }
 
