45716f7b97d98cdba5704f7485dfa54b936597ca
pinzonjulian <julianpinzoneslava@gmail.com>
2016-02-11 20:52:10 -0500

    First commit. Create landing folder with router config, module and html template

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+
+
+// var sass = require('gulp-sass');
+// var autoprefixer = require('gulp-autoprefixer');
+// var sourcemaps   = require('gulp-sourcemaps');
+// var cssnano      = require('gulp-cssnano');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var runSequence = require('run-sequence')
+
+
+// gulp.task('sass', function (){
+//   return gulp.src('app/styles/main.sass')
+//     .pipe(sourcemaps.init())
+//     .pipe(sass())
+//     .pipe(autoprefixer())
+//     .pipe(cssnano())
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest('app/css'))
+//     .pipe(reload({stream:true}))
+// })
+
+// gulp.task('distcss', function (){
+//   return gulp.src('app/styles/button-animations.sass')
+//     .pipe(sass())
+//     .pipe(autoprefixer())
+//     .pipe(cssnano())
+//     .pipe(gulp.dest('dist/'))
+// })
+
+// gulp.task('distjs', function (){
+// 	return gulp.src('app/js/button-animations.js')
+// 	.pipe(gulp.dest('dist/'))
+// })
+
+
+
+gulp.task('browserSync', function (){
+	browserSync({
+		server:{
+			baseDir: 'src'
+		}
+	})
+})
+
+
+gulp.task('watch', ['browserSync'], function (){
+  	// gulp.watch('src/styles/**/*.sass', ['sass']);
+	gulp.watch('src/*.html', reload);
+	gulp.watch('src/app/**/*.js', reload)
+})
+
+gulp.task('default', function(callback){
+	runSequence(
+		['browserSync', 'watch']
+	)
+})
---
+++ src/app/app.module.js
@@ -0,0 +1,16 @@
+(function (){
+  'use strict'
+
+  angular
+    .module('app', [
+      // Angular Modules
+      'ngRoute',
+
+      // Third party modules
+      'firebase',
+
+      // Custom Modules
+      'app.landing'
+    ]);
+
+})();
