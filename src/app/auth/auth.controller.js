74ed2b8434cfc750351abe7239657474cffe5d74
pinzonjulian <julianpinzoneslava@gmail.com>
2016-02-13 12:21:14 -0500

    Add code for auth.controller

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -9,11 +9,19 @@
 
   function AuthController ($firebaseAuth) {
     var vm = this;
+    var firebaseReference = new Firebase('https://jperestaurantapp.firebaseio.com/');
+    var firebaseAuthObject = $firebaseAuth(firebaseReference);
+
+    var user = {
+      email: '',
+      password: ''
+    }
 
     vm.register = register;
 
     function register (user){
       // will log the user in
+      return firebaseAuthObject.$createUser(user);
     }
 
   }
