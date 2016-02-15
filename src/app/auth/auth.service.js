9647e31691fc615901cd0798b287cd237da50cf7
pinzonjulian <julianpinzoneslava@gmail.com>
2016-02-15 14:57:56 -0500

    Create the authService

---
+++ src/app/auth/auth.service.js
@@ -0,0 +1,17 @@
+(function (){
+  'use strict';
+
+  angular
+    .module('app.auth')
+    .factory('authService', authService);
+
+    auth.Service.$inject('')
+
+  function authService() {
+    var service = {}
+    return service;
+  }
+
+  //===================
+
+})();
