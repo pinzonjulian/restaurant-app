5b79db5191be25a00941d805a586faef7aae6ea3
pinzonjulian <julianpinzoneslava@gmail.com>
2016-02-15 15:18:26 -0500

    Create authService.firebaseAuthObject to handle viewing waitList only if user is logged in

--- src/app/auth/auth.service.js
+++ src/app/auth/auth.service.js
@@ -12,6 +12,7 @@
     var firebaseAuthObject = $firebaseAuth(firebaseDataService.root)
 
     var service = {
+      firebaseAuthObject: firebaseAuthObject,
       register: register, 
       login : login,
       logout : logout, 
