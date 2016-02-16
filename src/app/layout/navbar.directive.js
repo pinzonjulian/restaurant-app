bd7a77af6fcf13ae95cfbcf58a0baeeac6bc6cc5
pinzonjulian <julianpinzoneslava@gmail.com>
2016-02-16 10:55:28 -0500

    Add template gz-navbar directive

---
+++ src/app/layout/navbar.directive.js
@@ -0,0 +1,15 @@
+(function (){
+  'use strict';
+
+  angular
+    .module('app.layout')
+    .directive('gzNavbar', gzNavbar)
+
+  function gzNavbar() {
+    return {
+      templateUrl : 'app/layout/navbar.html',
+      restrict : 'E'
+    }
+  }
+
+})();
