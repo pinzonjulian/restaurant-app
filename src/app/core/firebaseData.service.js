61099ce6b2ec476e65e891e8d7617f5f016b6fcf
pinzonjulian <julianpinzoneslava@gmail.com>
2016-02-16 15:55:00 -0500

    add authService.sendWelcomeEmail

--- src/app/core/firebaseData.service.js
+++ src/app/core/firebaseData.service.js
@@ -12,7 +12,9 @@
     var service = {
       root: root,
       users : root.child('users'),
+      emails : root.child('emails'),
       textMessages: root.child('textMessages')
+
 
     }
     return service;
